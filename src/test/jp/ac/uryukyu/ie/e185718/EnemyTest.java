fb13a3852e626dfe418d5ff145f64e883a961f33
Daichi kise <e185718@ie.u-ryukyu.ac.jp>
2018-11-12 00:15:57 +0900

    report3　step1

--- src/test/jp/ac/uryukyu/ie/e185718/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e185718/EnemyTest.java
@@ -11,7 +11,8 @@ class EnemyTest {
         int hitpoint = 10;
         Hero hero = new Hero("勇者", hitpoint, 5);
         Enemy enemy = new Enemy("スライム", 0, 3);
-        for(int i = 5;i > 0;--i) {
+        enemy.dead = true;
+        for(int i = 0;i < 5;++i) {
             enemy.attack(hero);
         }
         assertEquals(hitpoint,hero.hitPoint);
